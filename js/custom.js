b70b9d685cecf34a7539b7990528dbb0e408d24c
Andrew R. Edelson <aredelso@syr.edu>
2016-09-13 16:15:56 -0700

    first commit

---
+++ js/custom.js
@@ -0,0 +1,33 @@
+$(document).ready(function() {
+
+    var sideslider = $('[data-toggle=collapse-side]');
+    var sel = sideslider.attr('data-target');
+    var sel2 = sideslider.attr('data-target-2');
+    sideslider.click(function(event){
+        $(sel).toggleClass('in');
+        $(sel2).toggleClass('out');
+    });
+
+    // Add smooth scrolling to all links
+    $("a").on('click', function(event) {
+
+        // Make sure this.hash has a value before overriding default behavior
+        if (this.hash !== "") {
+            // Prevent default anchor click behavior
+            event.preventDefault();
+
+            // Store hash
+            var hash = this.hash;
+
+            // Using jQuery's animate() method to add smooth page scroll
+            // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 800, function(){
+
+                // // Add hash (#) to URL when done scrolling (default click behavior)
+                // window.location.hash = hash;
+            });
+        } // End if
+    });
+});
